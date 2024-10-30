52a970fa55b84a3c603c9681caa3f07c0658fc1f
Matteo Casu <matteo.casu91@gmail.com>
2024-10-30 16:14:51 +0100

    Initial Commit

---
+++ EssentialProducts/EssentialProducts.h
@@ -0,0 +1,18 @@
+//
+//  EssentialProducts.h
+//  EssentialProducts
+//
+//  Created by Matteo Casu on 30/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialProducts.
+FOUNDATION_EXPORT double EssentialProductsVersionNumber;
+
+//! Project version string for EssentialProducts.
+FOUNDATION_EXPORT const unsigned char EssentialProductsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialProducts/PublicHeader.h>
+
+

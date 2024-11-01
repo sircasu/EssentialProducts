db2a9b0fe675cc1645df85facc24c85c41ef1652
Matteo Casu <matteo.casu91@gmail.com>
2024-11-01 21:23:08 +0100

    `RemoteProductsLoader` request data on `load`

--- EssentialProductsTests/RemoteProductsLoaderTests.swift
+++ EssentialProductsTests/RemoteProductsLoaderTests.swift
@@ -9,18 +9,29 @@ import XCTest
 
 final class RemoteProductsLoader {
     
+    private var client: HTTPClient
+    private var url: URL
+    
+    init(client: HTTPClient, url: URL = URL(string: "https://example.com/products")!) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
 }
 
 protocol HTTPClient {
-    func get()
+    func get(from: URL)
 }
 
 final class HTTPClientSpy: HTTPClient {
     
     var requestedURL: URL?
     
-    func get() {
-        
+    func get(from url: URL) {
+        self.requestedURL = url
     }
 }
 
@@ -29,9 +40,21 @@ final class RemoteProductsLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
     
         let client = HTTPClientSpy()
-        
-        let sut = RemoteProductsLoader()
+
+        _ = RemoteProductsLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    
+    func test_load_requestDataFromURL() {
+        
+        let exampleURL = URL(string: "https://an-example.com/products")!
+        let client = HTTPClientSpy()
+        let sut = RemoteProductsLoader(client: client, url: exampleURL)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, exampleURL)
     }
 }

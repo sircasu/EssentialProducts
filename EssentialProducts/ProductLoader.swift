f33cd13f0b5ebca2b4a12fe6d58c150f49f367d7
Matteo Casu <matteo.casu91@gmail.com>
2024-11-01 19:28:54 +0100

    Create ProductLoader protocol

--- EssentialProducts/ProductLoader.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ProductLoader.swift
-//  EssentialProducts
-//
-//  Created by Matteo Casu on 01/11/24.
-//
-
-import Foundation
---
+++ EssentialProducts/ProductsLoader.swift
@@ -0,0 +1,13 @@
+//
+//  ProductsLoader.swift
+//  EssentialProducts
+//
+//  Created by Matteo Casu on 01/11/24.
+//
+
+import Foundation
+
+protocol ProductsLoader {
+    typealias Result = Swift.Result<[ProductItem], Error>
+    func load(completion: @escaping (Result) -> Void)
+}

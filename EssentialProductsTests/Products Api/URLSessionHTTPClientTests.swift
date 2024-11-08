e4219ce0774ab322551e2f4e7d8b9ef2c67daf71
Matteo Casu <matteo.casu91@gmail.com>
2024-11-08 11:57:53 +0100

    fix setup/tearDown override definition

--- EssentialProductsTests/Products Api/URLSessionHTTPClientTests.swift
+++ EssentialProductsTests/Products Api/URLSessionHTTPClientTests.swift
@@ -33,12 +33,12 @@ class URLSessionHTTPClient {
 
 final class URLSessionHTTPClientTests: XCTestCase {
 
-    override class func setUp() {
+    override func setUp() {
         super.setUp()
         URLProtocolStub.startInterceptingRequests()
     }
     
-    override class func tearDown() {
+    override func tearDown() {
         super.tearDown()
         URLProtocolStub.stopInterceptingRequests()
     }

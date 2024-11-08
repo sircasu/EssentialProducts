b70029089b93d21fd866a7e031cc2af335e131da
Matteo Casu <matteo.casu91@gmail.com>
2024-11-08 10:02:50 +0100

    Add `trackForMemoryLeak` method

--- EssentialProductsTests/Products Api/URLSessionHTTPClientTests.swift
+++ EssentialProductsTests/Products Api/URLSessionHTTPClientTests.swift
@@ -83,7 +83,15 @@ final class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
     
     func makeSUT() -> URLSessionHTTPClient {
-        return URLSessionHTTPClient()
+        let sut = URLSessionHTTPClient()
+        trackForMemoryLeak(sut)
+        return sut
+    }
+    
+    private func trackForMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated, Potential memory leak.", file: file, line: line)
+        }
     }
     
     private class URLProtocolStub: URLProtocol {

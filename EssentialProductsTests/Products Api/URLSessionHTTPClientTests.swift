53f365fae7ab9b0ab42ff80b4b1f771bd153018d
Matteo Casu <matteo.casu91@gmail.com>
2024-11-09 08:37:37 +0100

    Move `URLSessionHTTPClient` to its own file

--- EssentialProductsTests/Products Api/URLSessionHTTPClientTests.swift
+++ EssentialProductsTests/Products Api/URLSessionHTTPClientTests.swift
@@ -8,31 +8,6 @@
 import XCTest
 import EssentialProducts
 
-class URLSessionHTTPClient: HTTPClient {
-    
-    let session: URLSession
-    
-    struct UnexpectedError: Error {}
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-        
-        session.dataTask(with: url) { data, response, error in
-            
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            } else {
-                completion(.failure(UnexpectedError()))
-            }
-        }.resume()
-    }
-}
-
 final class URLSessionHTTPClientTests: XCTestCase {
 
     override func setUp() {

adf2126667d72e389132037fd34ce9e37c911e87
Matteo Casu <matteo.casu91@gmail.com>
2024-11-22 08:40:36 +0100

    `save` command ask to delete old cache

--- EssentialProductsTests/Products Cache/CacheProductsUseCaseTests.swift
+++ EssentialProductsTests/Products Cache/CacheProductsUseCaseTests.swift
@@ -9,12 +9,25 @@ import XCTest
 
 
 class LocalProductsLoader {
-    init(store: ProductStore) {}
+    
+    let store: ProductStore
+    
+    init(store: ProductStore) {
+        self.store = store
+    }
+    
+    func save() {
+        store.delete()
+    }
 }
 
 class ProductStore {
     
     var deleteCallCount = 0
+    
+    func delete() {
+        deleteCallCount += 1
+    }
 }
 
 
@@ -23,8 +36,19 @@ final class CacheProductsUseCaseTests: XCTestCase {
     func test_init_doesNotRequestToDeleteCache() {
     
         let store = ProductStore()
-        let sut = LocalProductsLoader(store: store)
+        let _ = LocalProductsLoader(store: store)
         
         XCTAssertEqual(store.deleteCallCount, 0)
     }
+    
+    func test_save_requestToDeleteCache() {
+        
+        let store = ProductStore()
+        let sut = LocalProductsLoader(store: store)
+    
+        sut.save()
+        
+        XCTAssertEqual(store.deleteCallCount, 1)
+    }
+
 }

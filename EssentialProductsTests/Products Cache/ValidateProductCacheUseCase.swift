130eabe44d4325904f4af1d7a96d564ed2fa56db
Matteo Casu <matteo.casu91@gmail.com>
2024-12-09 07:49:24 +0100

    LocalProductLoader does not message store upon creation (before validating the cached product)

---
+++ EssentialProductsTests/Products Cache/ValidateProductCacheUseCase.swift
@@ -0,0 +1,31 @@
+//
+//  ValidatwProductCacheUseCase.swift
+//  EssentialProductsTests
+//
+//  Created by Matteo Casu on 09/12/24.
+//
+
+import XCTest
+import EssentialProducts
+
+final class ValidateProductCacheUseCase: XCTestCase {
+    
+    func test_init_doesNotMessageStoreUponCreation() {
+
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalProductsLoader, ProductStoreSpy) {
+        let store = ProductStoreSpy()
+        let sut = LocalProductsLoader(store: store, currentDate: currentDate)
+        
+        trackForMemoryLeak(sut, file: file, line: line)
+        trackForMemoryLeak(store, file: file, line: line)
+        
+        return (sut: sut, store: store)
+    }
+}

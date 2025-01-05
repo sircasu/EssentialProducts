e6ee4b63bec01f8dcc094493fade93ff6408e8b6
Matteo Casu <matteo.casu91@gmail.com>
2025-01-05 20:30:41 +0100

    Extract reusable `ProductStoreSpecs` helper methods to a shared file, so it can be used by any `ProductStore` implementation test

--- EssentialProductsTests/Products Cache/CodableProductStoreTests.swift
+++ EssentialProductsTests/Products Cache/CodableProductStoreTests.swift
@@ -223,64 +223,6 @@ final class CodableProductStoreTests: XCTestCase, FailableFeedStoreSpecs {
         return sut
     }
     
-    @discardableResult
-    private func insert(_ cache: (products: [LocalProductItem], timestamp: Date), to sut: ProductStore, file: StaticString = #filePath, line: UInt = #line) -> Error? {
-        
-        let exp = expectation(description: "Wait for completion")
-        
-        var insertionError: Error?
-        sut.insert(cache.products, timestamp: cache.timestamp) { receivedInsertionError in
-            XCTAssertNil(insertionError, "Expected products to be inserted successfully", file: file, line: line)
-            exp.fulfill()
-            insertionError = receivedInsertionError
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-        return insertionError
-    }
-    
-    @discardableResult
-    func deleteCache(from sut: ProductStore, file: StaticString = #filePath, line: UInt = #line) -> Error? {
-        
-        let exp = expectation(description: "Wait for completion")
-        var deletionError: Error?
-        sut.deleteCachedProducts { receivedDeletionError in
-            exp.fulfill()
-            deletionError = receivedDeletionError
-        }
-        wait(for: [exp], timeout: 1.0)
-        return deletionError
-    }
-    
-    private func expect(_ sut: ProductStore, toRetrieve expectedResult: RetrievalCachedProductResult, file: StaticString = #filePath, line: UInt = #line) {
-
-        let exp = expectation(description: "Wait for completion")
-        
-        sut.retrieve { retrievedResult in
-            
-            switch(expectedResult, retrievedResult) {
-            case (.empty, .empty),
-                 (.failure, .failure):
-                break
-            case let (.found(expected, expectedTimestamp), .found(retrieved, retrievedTimestamp)):
-                XCTAssertEqual(expected, retrieved, file: file, line: line)
-                XCTAssertEqual(expectedTimestamp, retrievedTimestamp, file: file, line: line)
-        
-            default: XCTFail("Expected to retrieve \(expectedResult) got \(retrievedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-    }
-    
-    private func expect(_ sut: ProductStore, toRetrieveTwice expectedResult: RetrievalCachedProductResult, file: StaticString = #filePath, line: UInt = #line) {
-        
-        expect(sut, toRetrieve: expectedResult)
-        expect(sut, toRetrieve: expectedResult)
-    }
-    
     private func testSpecificStoreURL() -> URL {
         return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
     }

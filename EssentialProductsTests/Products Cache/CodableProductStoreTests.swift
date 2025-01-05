f77a7e7f1b611856a36f014a9c7c1f1933dd014e
Matteo Casu <matteo.casu91@gmail.com>
2025-01-05 20:22:52 +0100

    Create `ProductStoreSpecs` (and move  to its own file) that must be implemented by any `ProductStore` imlementation to guarantee it meets specs

--- EssentialProductsTests/Products Cache/CodableProductStoreTests.swift
+++ EssentialProductsTests/Products Cache/CodableProductStoreTests.swift
@@ -8,40 +8,7 @@
 import XCTest
 import EssentialProducts
 
-protocol ProductStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnEmptyCache()
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
-    
-    func test_insert_deliversNoErrorOnEmptyCache()
-    func test_insert_deliversNoErrorOnNonEmptyCache()
-    func test_insert_overridesExistingCache()
-    
-    func test_delete_deliversNoErrorOnEmptyCache()
-    func test_delete_hasNoSideEffectsOnEmptyCache()
-    func test_delete_deliversNoErrorOnNonEmptyCache()
-    func test_delete_leavesCacheEmptyOnNonEmptyCache()
-    
-    func test_storeSideEffects_runSerially()
-}
-
-protocol FailableRetrieveProductStoreSpecs {
-    func test_retrieve_deliversErrorOnInvalidData()
-    func test_retrieve_hasNoSideEffectsOnFailure()
-}
-
-protocol FailableInsertProductStoreSpecs {
-    func test_insert_deliversErrorOnInsertionError()
-    func test_insert_hasNoSideEffectsOnInsertionError()
-}
-
-protocol FailableDeleteProductStoreSpecs {
-    func test_delete_deliversErrorOnDeletionError()
-    func test_delete_hasNoSideEffectOnDeletionError()
-}
-
-final class CodableProductStoreTests: XCTestCase {
+final class CodableProductStoreTests: XCTestCase, FailableFeedStoreSpecs {
     
     override func setUp() {
         super.setUp()
---
+++ EssentialProductsTests/Products Cache/ProductStoreSpecs.swift
@@ -0,0 +1,43 @@
+//
+//  ProductStoreSpecs.swift
+//  EssentialProductsTests
+//
+//  Created by Matteo Casu on 05/01/25.
+//
+
+import Foundation
+
+protocol ProductStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache()
+    func test_retrieve_hasNoSideEffectsOnEmptyCache()
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
+    
+    func test_insert_deliversNoErrorOnEmptyCache()
+    func test_insert_deliversNoErrorOnNonEmptyCache()
+    func test_insert_overridesExistingCache()
+    
+    func test_delete_deliversNoErrorOnEmptyCache()
+    func test_delete_hasNoSideEffectsOnEmptyCache()
+    func test_delete_deliversNoErrorOnNonEmptyCache()
+    func test_delete_leavesCacheEmptyOnNonEmptyCache()
+    
+    func test_storeSideEffects_runSerially()
+}
+
+protocol FailableRetrieveProductStoreSpecs: ProductStoreSpecs {
+    func test_retrieve_deliversErrorOnInvalidData()
+    func test_retrieve_hasNoSideEffectsOnFailure()
+}
+
+protocol FailableInsertProductStoreSpecs: ProductStoreSpecs {
+    func test_insert_deliversErrorOnInsertionError()
+    func test_insert_hasNoSideEffectsOnInsertionError()
+}
+
+protocol FailableDeleteProductStoreSpecs: ProductStoreSpecs {
+    func test_delete_deliversErrorOnDeletionError()
+    func test_delete_hasNoSideEffectOnDeletionError()
+}
+
+typealias FailableFeedStoreSpecs = FailableDeleteProductStoreSpecs & FailableInsertProductStoreSpecs & FailableRetrieveProductStoreSpecs

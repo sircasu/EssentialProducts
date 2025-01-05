6d94f404e3a4fb2aa26b70a2dae2927893c7e521
Matteo Casu <matteo.casu91@gmail.com>
2025-01-05 23:07:18 +0100

    Add empty CoreDataProductStoreTests file

---
+++ EssentialProductsTests/Products Cache/CoreDataProductStoreTests.swift
@@ -0,0 +1,84 @@
+//
+//  CoreDataProductStoreTests.swift
+//  EssentialProductsTests
+//
+//  Created by Matteo Casu on 05/01/25.
+//
+
+import XCTest
+
+final class CoreDataProductStoreTests: XCTestCase, FailableProductStoreSpecs {
+    func test_delete_deliversErrorOnDeletionError() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectOnDeletionError() {
+        
+    }
+    
+    func test_insert_deliversErrorOnInsertionError() {
+        
+    }
+    
+    func test_insert_hasNoSideEffectsOnInsertionError() {
+        
+    }
+    
+    func test_retrieve_deliversErrorOnInvalidData() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+        
+    }
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesExistingCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_leavesCacheEmptyOnNonEmptyCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
+    
+}

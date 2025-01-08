5a4af44d37689427ba53b33c3945e73ee6975bef
Matteo Casu <matteo.casu91@gmail.com>
2025-01-08 19:25:37 +0100

    Add and setup new `EssentialProductsCacheIntegrationTests` file

---
+++ EssentialProductsCacheIntegrationTests/EssentialProductsCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialProductsCacheIntegrationTests.swift
+//  EssentialProductsCacheIntegrationTests
+//
+//  Created by Matteo Casu on 08/01/25.
+//
+
+import XCTest
+
+final class EssentialProductsCacheIntegrationTests: XCTestCase {
+    
+}

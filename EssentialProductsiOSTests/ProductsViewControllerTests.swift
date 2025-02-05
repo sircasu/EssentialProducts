9cc3fa05bf6cf39b76025bea643e70b7f29c8c4c
Matteo Casu <matteo.casu91@gmail.com>
2025-02-05 08:55:46 +0100

    add ´ProductsViewControllerTests´ file and its related scheme

---
+++ EssentialProductsiOSTests/ProductsViewControllerTests.swift
@@ -0,0 +1,12 @@
+//
+//  ProductsViewControllerTests.swift
+//  EssentialProductsiOSTests
+//
+//  Created by Matteo Casu on 05/02/25.
+//
+
+import XCTest
+
+final class ProductsViewControllerTests: XCTestCase {
+
+}

3329c25f629988fc04cd0bab3e9adb02238522d9
Matteo Casu <matteo.casu91@gmail.com>
2025-02-05 09:00:54 +0100

    `init` action does not load products

--- EssentialProductsiOSTests/ProductsViewControllerTests.swift
+++ EssentialProductsiOSTests/ProductsViewControllerTests.swift
@@ -7,6 +7,23 @@
 
 import XCTest
 
+final class ProductsViewController {
+    
+    init(loader: ProductsViewControllerTests.LoaderSpy) {}
+}
+
 final class ProductsViewControllerTests: XCTestCase {
 
+    func test_init_doesNotLoadProducts() {
+        
+        let loader = LoaderSpy()
+        let _ = ProductsViewController(loader: loader)
+        
+        XCTAssertEqual(loader.callCount, 0)
+    }
+    
+    
+    class LoaderSpy {
+        var callCount = 0
+    }
 }

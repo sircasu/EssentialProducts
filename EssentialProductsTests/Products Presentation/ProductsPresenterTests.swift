7b0f2887517a91e94ba18900985e5ec5a8120f2d
Matteo Casu <matteo.casu91@gmail.com>
2025-03-19 15:47:36 +0100

    `ProductsPresenter` display on `didStartLoadingProducts`

--- EssentialProductsTests/Products Presentation/ProductsPresenterTests.swift
+++ EssentialProductsTests/Products Presentation/ProductsPresenterTests.swift
@@ -7,9 +7,28 @@
 
 import XCTest
 
-final class ProductsPresenter {
-    init(view: Any) {
+struct ProductsErrorViewModel {
+    var message: String?
+    
+    static var noError: ProductsErrorViewModel {
+        return ProductsErrorViewModel(message: nil)
+    }
+}
 
+protocol ProductsErrorView {
+    func display(_ viewModel: ProductsErrorViewModel)
+}
+
+final class ProductsPresenter {
+    
+    var productsErrorView: ProductsErrorView
+    
+    init(productsErrorView: ProductsErrorView) {
+        self.productsErrorView = productsErrorView
+    }
+    
+    func didStartLoadingProducts() {
+        productsErrorView.display(.noError)
     }
 }
 
@@ -22,20 +41,38 @@ final class ProductsPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages on init")
     }
     
+    func test_didStartLoadingProducts_displayNoErrorMessages() {
+        
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingProducts()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+    }
+    
     
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ProductsPresenter, view: ViewSpy) {
         
         let view = ViewSpy()
-        let sut = ProductsPresenter(view: view)
+        let sut = ProductsPresenter(productsErrorView: view)
         trackForMemoryLeak(view, file: file, line: line)
         trackForMemoryLeak(sut, file: file, line: line)
         return (sut, view)
     }
     
-    private class ViewSpy {
+    private class ViewSpy: ProductsErrorView {
+
         
-        let messages = [Any]()
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+        private(set) var messages = [Message]()
+        
+        func display(_ viewModel: ProductsErrorViewModel) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
     }
 }
